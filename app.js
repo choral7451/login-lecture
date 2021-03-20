92080055c5e9a231cfb39956fb255f29b4e1de4b
임성준 <jun@MacBookPro.local>
2021-03-20 12:41:18 +0900

    깃 저장소 초기화

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// 모듈
+const express = require("express");
+const app = express();
+
+// 라우팅
+const home = require("./routes/home"); // home 이란것을 설정 // 위치한 경로의 js(index.js)파일을 home로 받아들임
+
+//앱 세팅
+app.set("views", "./views");   // (뷰스 세팅, 폴더사용)
+app.set("view engine", "ejs");  //  (뷰 엔진을, ejs으로 해석) * ejs(html이랑 비슷) - 뷰엔진
+
+app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드 (내보낸 router 받기위함)// "/" (루트)경로로 오게되면 홈으로 이동
+
+module. exports = app;
---
+++ bin/www.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const app = require("../app")
+const PORT = 3000; // PORT를 3000으로 정의 함
+
+app.listen(PORT, () => {
+  console.log("서버 가동");
+});   
