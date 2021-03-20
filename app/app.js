32f22953d119131de526443601f7461fd81f5f41
임성준 <jun@MacBookPro.local>
2021-03-20 15:44:14 +0900

    nodemon 적용

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require("./src/routes/home"); // home 이란것을 설정 // 위치
 //앱 세팅
 app.set("views", "./src/views");   // (뷰스 세팅, 폴더사용)
 app.set("view engine", "ejs");  //  (뷰 엔진을, ejs으로 해석) * ejs(html이랑 비슷) - 뷰엔진
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드 (내보낸 router 받기위함)// "/" (루트)경로로 오게되면 홈으로 이동
 
